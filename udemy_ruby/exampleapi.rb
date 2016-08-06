e7bea9c856f0a83912064f4c3c69394cf9ce9aba
Jerome Nixon <jerome.nixon@ihg.com>
2016-08-06 16:47:44 -0400

    new rails app

--- udemy_ruby/exampleapi.rb
+++ udemy_ruby/exampleapi.rb
@@ -1,17 +1,21 @@
 require 'rubygems'
 require 'httparty'
-
+#
 # class EdutechionalResty
 #   include HTTParty
-#   base_uri "api.twitter.com/"
+#   base_uri "edutechional-resty.herokuapp.com/"
 #
 #   def posts
-#     self.class.get('/1.1/statuses/mentions_timeline.json')
+#     self.class.get('/posts.json')
 #   end
 # end
 #
 # edutechional_resty = EdutechionalResty.new
-# puts edutechional_resty.posts
+# # puts edutechional_resty.posts
+#
+# edutechional_resty.posts.each do |post|
+#   p "Title: #{post['title']} | Descripttion: #{post['description']}"
+# end
 
 response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow')
 
@@ -35,9 +39,10 @@ class StackExchange
   def users
     self.class.get('/2.2/users', @options)
   end
+ end
 end
 
 stack_exchange = StackExchange.new('stackoverflow', 1)
 
 # puts stack_exchange.questions
-puts stack_exchange.users
+# puts stack_exchange.users
