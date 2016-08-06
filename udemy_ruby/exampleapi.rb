933b14c6c2d712e75b143f721e69cec630855dfd
Jerome Nixon <jerome.nixon@ihg.com>
2016-08-05 20:54:09 -0400

    new ruby file

--- udemy_ruby/exampleapi.rb
+++ udemy_ruby/exampleapi.rb
@@ -15,4 +15,29 @@ require 'httparty'
 
 response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow')
 
-puts response.body
+# puts response.body
+# puts response.code
+# puts response.messege
+# puts response.headers.inspect
+
+class StackExchange
+  include HTTParty
+  base_uri 'api.stackexchange.com'
+
+  def initialize(service, page)
+    @options = {query: {site: service}}
+  end
+
+  def questions
+    self.class.get('/2.2/questions', @options)
+  end
+
+  def users
+    self.class.get('/2.2/users', @options)
+  end
+end
+
+stack_exchange = StackExchange.new('stackoverflow', 1)
+
+# puts stack_exchange.questions
+# puts stack_exchange.users
