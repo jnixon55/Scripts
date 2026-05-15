0b61ca956376ce10e7152cefcc549a9dc8b4c227
Jerome Nixon <jnixon55@gmail.com>
2026-05-15 07:03:25 -0400

    fix: added sshpass

--- Command_Cheatsheet.sh
+++ Command_Cheatsheet.sh
@@ -5,29 +5,32 @@ Command Cheat Sheet
 		###################################################################
 
 
-		#!/bin/bash
-		SERVERS=(
-		"server1.global"
-		"server6.global"
-		# add more here
-		)
+	#!/bin/bash
+	SERVERS=(
+	"server1.global"
+	"server6.global"
+	)
 
-		SESSION="servers"
-		tmux new-session -d -s $SESSION
+	SESSION="servers"
 
-		for i in "${!SERVERS[@]}"; do
-		if [ $i -eq 0 ]; then
-			tmux send-keys -t $SESSION "ssh ${SERVERS[$i]}" Enter
-		else
-			tmux split-window -t $SESSION
-			tmux send-keys -t $SESSION "ssh ${SERVERS[$i]}" Enter
-		fi
-		done
+	# Prompt once, don't echo the password
+	read -s -p "SSH Password: " SSHPASS
+	echo
+	export SSHPASS
 
-		# Optional: tile panes evenly
-		tmux select-layout -t $SESSION tiled
+	tmux new-session -d -s $SESSION
 
-		tmux attach -t $SESSION
+	for i in "${!SERVERS[@]}"; do
+	if [ $i -eq 0 ]; then
+		tmux send-keys -t $SESSION "export SSHPASS='$SSHPASS' && sshpass -e ssh ${SERVERS[$i]}" Enter
+	else
+		tmux split-window -t $SESSION
+		tmux send-keys -t $SESSION "export SSHPASS='$SSHPASS' && sshpass -e ssh ${SERVERS[$i]}" Enter
+	fi
+	done
+
+	tmux select-layout -t $SESSION tiled
+	tmux attach -t $SESSION
 
 			###############################################
 			###Display date and time in history command ###
